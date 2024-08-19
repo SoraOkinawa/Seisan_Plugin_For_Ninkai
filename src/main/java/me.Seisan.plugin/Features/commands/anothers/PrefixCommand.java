195caeab55bbc2941fd6beb4f863260baf6a449b
Mirafii <mirafii91@gmail.com>
2024-08-19 23:01:36 +0200

    Remove output

--- src/main/java/me.Seisan.plugin/Features/commands/anothers/PrefixCommand.java
+++ src/main/java/me.Seisan.plugin/Features/commands/anothers/PrefixCommand.java
@@ -38,7 +38,6 @@ public class PrefixCommand extends Command {
     }
 
     public static String getPlayerDefaultPrefix(Player p) {
-        System.out.println(prefix.getOrDefault(p.getUniqueId(), ""));
         return prefix.getOrDefault(p.getUniqueId(), "");
     }
 
