6a0a65ade16a9b7ab0306cdeedd32dd4f81aeab9
TimOn <timcola12@gmail.com>
2024-09-01 20:56:53 +0200

    Prier le Rikudo

--- src/main/java/me.Seisan.plugin/Features/commands/anothers/PrierCommand.java
+++ src/main/java/me.Seisan.plugin/Features/commands/anothers/PrierCommand.java
@@ -1,9 +1,11 @@
 package me.Seisan.plugin.Features.commands.anothers;
 
 import me.Seisan.plugin.Features.PlayerData.PlayerInfo;
+import me.Seisan.plugin.Features.skill.SkillLevel;
 import me.Seisan.plugin.Main.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 
 import java.util.*;
 
@@ -24,24 +26,41 @@ public class PrierCommand extends Command {
                 player.sendMessage("§4HRP : §7Il faut faire la commande /priere <message>");
             }
             action = Arrays.toString(split);
-            if (action.length() >= 1) {
-                if (action.length() >= 200) {
-                    waitingForAction.put(player.getUniqueId(), split);
-                    player.sendMessage("§4HRP : §b Votre Message est enregistré pour la suite !");
-                } else {
-                    waitingForAction.remove(player.getUniqueId());
-                    player.sendMessage("§bHRP: Votre message a bien été envoyé !");
-                }
+            if (action.length() >= 1 && !(split[0].equals("send"))) {
+                waitingForAction.put(player.getUniqueId(), split);
+                player.sendMessage("§4HRP : §b Votre Message est enregistré pour la suite !");
             }
+
+            if (split[0].equals("send")) {
+                sendPrier(player, split);
+                List<String> completions = new ArrayList<>();
+                for (SkillLevel skillLevel : SkillLevel.values()) {
+                    completions.add(skillLevel.getCharName());
+                }
+
+
+            }
+
+
         }
-
-
     }
 
 
     @Override
     protected List<String> myOnTabComplete(CommandSender sender, org.bukkit.command.Command command, String
-            label, String[] split) {
-        return List.of();
+            label, String[] args) {
+        if (args.length == 1 && waitingForAction.containsKey(((Player) sender).getUniqueId())) {
+            return StringUtil.copyPartialMatches(args[0], Collections.singletonList("send"), new ArrayList<>());
+        } else if (args.length > 1 && !(args[0].equals("send"))) {
+
+            List<String> completions = new ArrayList<>();
+            return StringUtil.copyPartialMatches(args[args.length - 1], completions, new ArrayList<>());
+        }
+        return Collections.emptyList();
+    }
+
+
+    private void sendPrier(Player player, String[] split) {
+        player.sendMessage("§bHRP: Votre message a bien été envoyé !");
     }
 }
