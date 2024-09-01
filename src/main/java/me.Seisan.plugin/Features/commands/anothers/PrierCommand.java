f23c923664bea86848151070cced0ac58d8db76e
TimOn <timcola12@gmail.com>
2024-09-01 14:46:05 +0200

    Priere

--- src/main/java/me.Seisan.plugin/Features/commands/anothers/PrierCommand.java
+++ src/main/java/me.Seisan.plugin/Features/commands/anothers/PrierCommand.java
@@ -1,28 +1,47 @@
 package me.Seisan.plugin.Features.commands.anothers;
 
+import me.Seisan.plugin.Features.PlayerData.PlayerInfo;
 import me.Seisan.plugin.Main.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import oshi.jna.platform.mac.SystemB;
 
-import java.util.List;
+import java.util.*;
 
 public class PrierCommand extends Command {
 
+    private final Map<UUID, String[]> waitingForAction = new HashMap<>();
+
+
     @Override
     public void myOnCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] split) {
-
+        String action;
+        Player player;
         if (sender instanceof Player) {
-            Player player = (Player) sender;//message d'erreur si il y a pas d'argument dans la commande
+            player = (Player) sender;
+            PlayerInfo playerInfo = PlayerInfo.getPlayerInfo(player);
+            //message d'erreur si il y a pas d'argument dans la commande
             if (split.length == 0) {
                 player.sendMessage("§4HRP : §7Il faut faire la commande /priere <message>");
             }
-
+            action = Arrays.toString(split);
+            if (action.length() >= 1) {
+                if (action.length() >= 200) {
+                    waitingForAction.put(player.getUniqueId(), split);
+                    player.sendMessage("§4HRP : §b Votre Message est enregistré pour la suite !");
+                } else {
+                    waitingForAction.remove(player.getUniqueId());
+                    player.sendMessage("§bHRP: Votre message a bien été envoyé !");
+                }
+            }
         }
+
+
     }
 
+
     @Override
-    protected List<String> myOnTabComplete(CommandSender sender, org.bukkit.command.Command command, String label, String[] split) {
+    protected List<String> myOnTabComplete(CommandSender sender, org.bukkit.command.Command command, String
+            label, String[] split) {
         return List.of();
     }
 }
