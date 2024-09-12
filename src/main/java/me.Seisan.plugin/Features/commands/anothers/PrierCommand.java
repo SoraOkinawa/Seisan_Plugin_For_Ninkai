ce6ddde1d40c8d36d8ee0b6d35206ecf12fe25d6
TimOn <timcola12@gmail.com>
2024-09-12 20:33:54 +0200

    Commande priere pour le Rikudo

--- src/main/java/me.Seisan.plugin/Features/commands/anothers/PrierCommand.java
+++ src/main/java/me.Seisan.plugin/Features/commands/anothers/PrierCommand.java
@@ -1,13 +1,18 @@
 package me.Seisan.plugin.Features.commands.anothers;
 
-import me.Seisan.plugin.Features.PlayerData.PlayerInfo;
 import me.Seisan.plugin.Features.skill.SkillLevel;
+import me.Seisan.plugin.Features.utils.DiscordWebhook;
 import me.Seisan.plugin.Main.Command;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.bukkit.ChatColor.COLOR_CHAR;
 
 public class PrierCommand extends Command {
 
@@ -16,51 +21,70 @@ public class PrierCommand extends Command {
 
     @Override
     public void myOnCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] split) {
-        String action;
-        Player player;
+
+        Player player = (Player) sender;
         if (sender instanceof Player) {
-            player = (Player) sender;
-            PlayerInfo playerInfo = PlayerInfo.getPlayerInfo(player);
+
             //message d'erreur si il y a pas d'argument dans la commande
             if (split.length == 0) {
                 player.sendMessage("§4HRP : §7Il faut faire la commande /priere <message>");
-            }
-            action = Arrays.toString(split);
-            if (action.length() >= 1 && !(split[0].equals("send"))) {
-                waitingForAction.put(player.getUniqueId(), split);
-                player.sendMessage("§aHRP : §b Votre Message est enregistré pour la suite !");
-            }
+            }             //si le joueur met sa prière sans l'envoyer
 
+            if (split.length >= 1 && !(split[0].equals("send"))) {
+                waitingForAction.get(split);
+                player.sendMessage("§aHRP : Votre prière: " + (Arrays.toString(split)) + " a été enregistré pour la suite !");
+            }
+            //Si le joueur envoi la prière avec le send
             if (split[0].equals("send")) {
-                sendPrier(player, split);
-                List<String> completions = new ArrayList<>();
-                for (SkillLevel skillLevel : SkillLevel.values()) {
-                    completions.add(skillLevel.getCharName());
+                if (waitingForAction.containsKey(player.getUniqueId())) {
+                    waitingForAction.get(player.getUniqueId());
+                    waitingForAction.remove(player.getUniqueId());
+                    sendLog(player, split);
+                    player.sendMessage("HRP : Votre prière:" + (Arrays.toString(split)) + " a été envoyé !");
+
+
                 }
-
-
             }
-
 
         }
     }
 
 
+
+
+    //Pour la complétion
     @Override
-    protected List<String> myOnTabComplete(CommandSender sender, org.bukkit.command.Command command, String
-            label, String[] args) {
-        if (args.length == 1 && waitingForAction.containsKey(((Player) sender).getUniqueId())) {
-            return StringUtil.copyPartialMatches(args[0], Collections.singletonList("send"), new ArrayList<>());
-        } else if (args.length > 1 && !(args[0].equals("send"))) {
+    protected List<String> myOnTabComplete(CommandSender sender, org.bukkit.command.Command command, String label, String[] split) {
+        if (split.length == 1 && waitingForAction.containsKey(((Player) sender).getUniqueId())) {
+            return StringUtil.copyPartialMatches(split[0], Collections.singletonList("send"), new ArrayList<>());
+        } else if (split.length > 1 && !split[0].equalsIgnoreCase("send")) {
 
             List<String> completions = new ArrayList<>();
-            return StringUtil.copyPartialMatches(args[args.length - 1], completions, new ArrayList<>());
+            for (SkillLevel skillLevel : SkillLevel.values()) {
+                completions.add(skillLevel.getCharName());
+            }
+            return StringUtil.copyPartialMatches(split[split.length - 1], completions, new ArrayList<>());
         }
         return Collections.emptyList();
     }
-
-
-    private void sendPrier(Player player, String[] split) {
-        player.sendMessage("§bHRP: Votre message a bien été envoyé !");
+    private void sendLog(Player player, String[] split){
+        DiscordWebhook webhook = new DiscordWebhook("https://discord.com/api/webhooks/1279553496244289556/nfnloSQZN9y6eqy_ANfFMvRuo_qK2XA1z-sWes5UntesPKs9t9pk5lq_jKHYWDoLbeFV");
+        webhook.setContent("Le joueur " + player.getDisplayName() + " à prié : " + (Arrays.toString(split)));
+        webhook.setUsername(ChatColor.stripColor(player.getDisplayName()) + " [" + player.getName() + "]");
+        try {
+            webhook.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
