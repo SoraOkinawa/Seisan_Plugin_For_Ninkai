72322d993b60d5ae6405ca8e6cb43c54d6998f76
TimOn <timcola12@gmail.com>
2024-11-08 18:08:09 +0100

    ./a allonger

--- src/main/java/me.Seisan.plugin/Features/commands/profil/ApparenceCommand.java
+++ src/main/java/me.Seisan.plugin/Features/commands/profil/ApparenceCommand.java
@@ -9,46 +9,50 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Helliot on 16/08/2018
  */
 public class ApparenceCommand extends Command {
-    private static Map<Player, String> halfWrittenMessage = new HashMap<>();
+    // Utiliser un Map pour stocker les messages en cours d'écriture par joueur
+    private static final Map<Player, StringBuilder> halfWrittenMessage = new HashMap<>();
 
     @Override
-    public void myOnCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] split) {
-        if(sender instanceof Player){
-            Player p = (Player) sender;
-            PlayerInfo pInfo = PlayerInfo.getPlayerInfo(p);
+    public void myOnCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            PlayerInfo playerInfo = PlayerInfo.getPlayerInfo(player);
 
-            if(split.length > 0){
+            if (args.length > 0) {
+                // Concaténer tous les mots du message en une seule chaîne
+                String message = String.join(" ", args).replace("&", "§");
 
-                String message = "";
-
-                for(String s : split){
-                    message = message.concat(s + " ");
+                // Vérifie si le message se termine par ">" pour savoir s'il est incomplet
+                if (message.endsWith(">")) {
+                    // Supprime le ">" et stocke le message comme une partie incomplète
+                    String incompleteMessage = message.substring(0, message.length() - 1);
+                    halfWrittenMessage.computeIfAbsent(player, k -> new StringBuilder()).append(incompleteMessage).append(" ");
+                    player.sendMessage("§CHRP : §7Message de l'apparence partiel enregistré. Continuez en tapant la suite avec la commande ./apparence.");
+                } else {
+                    // Si le message est complet, vérifier si on a un message stocké pour ce joueur
+                    if (halfWrittenMessage.containsKey(player)) {
+                        // Ajoute la partie finale au message complet stocké et l'envoie
+                        message = halfWrittenMessage.get(player).append(message).toString();
+                        halfWrittenMessage.remove(player);
+                    }
+                    playerInfo.setAppearance(message);
+                    player.sendMessage("§4HRP : §aVotre apparence est désormais : §2" + message);
                 }
-                message = message.replace("&","§");
-                pInfo.setAppearance(message);
-                p.sendMessage(ChatColor.GREEN + "Votre apparence est maintenant: * " + ChatColor.translateAlternateColorCodes('&', message) + ChatColor.GREEN + " *");
-            }else{
-                String apparence = "" + pInfo.getAppearance();
-                p.sendMessage(ChatColor.GREEN + "* " + ChatColor.translateAlternateColorCodes('&', apparence) + ChatColor.GREEN + " *");
-
             }
         }
     }
 
     @Override
-    protected List<String> myOnTabComplete(CommandSender sender, org.bukkit.command.Command command, String label, String[] split)
-    {
+    protected List<String> myOnTabComplete(CommandSender sender, org.bukkit.command.Command command, String label, String[] split) {
         return new ArrayList<>();
     }
+
     @Override
     protected boolean isOpOnly() {
         return false;
