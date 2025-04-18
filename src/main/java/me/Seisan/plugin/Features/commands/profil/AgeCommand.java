b7e8190ee1cc083adfd43a0a9fe104af4b5a7771
Anthony Tirel--Tartuffe <tony.tirell@gmail.com>
2025-04-18 15:02:04 +0200

    Adapt age command to fit permissions change

--- src/main/java/me/Seisan/plugin/Features/commands/profil/AgeCommand.java
+++ src/main/java/me/Seisan/plugin/Features/commands/profil/AgeCommand.java
@@ -17,27 +17,23 @@ import java.util.List;
 public class AgeCommand extends Command {
     @Override
     public void myOnCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] split) {
-        if(sender.isOp()){
-            if(split.length == 2){
-                Player target = sender.getServer().getPlayer(split[0]);
-                if(target != null){
-                    if(StringUtils.isNumeric(split[1])){
-                        int age = Integer.parseInt(split[1]);
-                        PlayerInfo tInfo = PlayerInfo.getPlayerInfo(target);
-                        tInfo.setAge(age);
-                        target.sendMessage(ChatColor.GRAY + "Vous avez maintenant " + age + " ans");
-                        sender.sendMessage(target.getDisplayName() + ChatColor.GRAY + " a maintenant " + age + " ans");
-                    }else{
-                        sender.sendMessage(ChatColor.RED + "L'age doit être un nombre positif");
-                    }
+        if(split.length == 2){
+            Player target = sender.getServer().getPlayer(split[0]);
+            if(target != null){
+                if(StringUtils.isNumeric(split[1])){
+                    int age = Integer.parseInt(split[1]);
+                    PlayerInfo tInfo = PlayerInfo.getPlayerInfo(target);
+                    tInfo.setAge(age);
+                    target.sendMessage(ChatColor.GRAY + "Vous avez maintenant " + age + " ans");
+                    sender.sendMessage(target.getDisplayName() + ChatColor.GRAY + " a maintenant " + age + " ans");
                 }else{
-                    sender.sendMessage(ChatColor.RED + "Ce joueur n'est pas connecté !");
+                    sender.sendMessage(ChatColor.RED + "L'age doit être un nombre positif");
                 }
             }else{
-                sender.sendMessage(ChatColor.RED + "Syntaxe: /age (joueur) (age)");
+                sender.sendMessage(ChatColor.RED + "Ce joueur n'est pas connecté !");
             }
         }else{
-            sender.sendMessage(ChatColor.RED + "Vous n'avez pas la permission !");
+            sender.sendMessage(ChatColor.RED + "Syntaxe: /age (joueur) (age)");
         }
     }
 
