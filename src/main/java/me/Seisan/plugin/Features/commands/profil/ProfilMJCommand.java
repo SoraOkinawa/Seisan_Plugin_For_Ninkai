85a4af84ca56ffd1e7b4289a2423f34a1af35a1d
Anthony Tirel--Tartuffe <tony.tirell@gmail.com>
2025-04-25 16:50:17 +0200

    Adapt ProfilMJCommand to new Permission

--- src/main/java/me/Seisan/plugin/Features/commands/profil/ProfilMJCommand.java
+++ src/main/java/me/Seisan/plugin/Features/commands/profil/ProfilMJCommand.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProfilMJCommand extends Command {
+    public static final String PERMISSION_SAVE = "ninkai.profil.mj.save";
+    public static final String PERMISSION_LOAD = "ninkai.profil.mj.load";
     @Override
     public void myOnCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] split) {
         if(sender instanceof Player) {
@@ -20,7 +22,7 @@ public class ProfilMJCommand extends Command {
             }
         }
         if(split.length == 2) {
-            if(split[0].equals("save")) {
+            if(split[0].equals("save") && sender.hasPermission(PERMISSION_SAVE)) {
                     PlayerInfo playerInfo = PlayerInfo.getPlayerInfo((Player) sender);
                     PlayerInfo profil = playerInfo.clone(split[1]);
 
@@ -32,7 +34,7 @@ public class ProfilMJCommand extends Command {
                     }
                     Main.getFicheMJ().put(split[1], profil);
             }
-            else if(split[0].equals("load")) {
+            else if(split[0].equals("load") && sender.hasPermission(PERMISSION_LOAD)) {
                 if(!Main.getFicheMJ().containsKey(split[1])) {
                     sender.sendMessage("§cHRP : §7La fiche n'est pas dispo");
                     return;
@@ -48,24 +50,16 @@ public class ProfilMJCommand extends Command {
     @Override
     protected List<String> myOnTabComplete(CommandSender sender, org.bukkit.command.Command command, String label, String[] split)
     {
-        if(sender instanceof Player) {
-            PlayerConfig pConfig = PlayerConfig.getPlayerConfig((Player)sender);
-            if(!sender.isOp() && !pConfig.isEncamode()) {
-                return null;
-            }
-        }
         List<String> completion = new ArrayList();
         if(split.length == 1) {
-            complete(completion, "save", split[0]);
-            complete(completion, "load", split[0]);
+            if (sender.hasPermission(PERMISSION_SAVE)) complete(completion, "save", split[0]);
+            if (sender.hasPermission(PERMISSION_LOAD)) complete(completion, "load", split[0]);
         }
-        if(split.length == 2) {
+        if(split.length == 2 && (sender.hasPermission(PERMISSION_SAVE) || sender.hasPermission(PERMISSION_LOAD))) {
             for(String test : Main.getFicheMJ().keySet()) {
                 complete(completion, test, split[1]);
             }
         }
         return completion;
     }
-
-
 }
