fcf27339774b81fea24429ae0ff4bdd326b2cb1b
Anthony Tirel--Tartuffe <tony.tirell@gmail.com>
2025-07-17 12:10:05 +0200

    Remove permanent /build command

--- src/main/java/me/Seisan/plugin/Features/commands/anothers/BuildCommand.java
+++ src/main/java/me/Seisan/plugin/Features/commands/anothers/BuildCommand.java
@@ -26,49 +26,30 @@ public class BuildCommand extends Command {
     @Override
 
     public void myOnCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] split) {
-        if (split.length == 2) {
-            Player target = Bukkit.getPlayer(split[1]);
+        if (split.length == 1) {
+            Player target = Bukkit.getPlayer(split[0]);
             if (target == null) {
                 sender.sendMessage("§7Le joueur n'est pas connecté.");
                 return;
             }
-            switch (split[0]) {
-                case "definitif":
-                    PlayerConfig pConfig = PlayerConfig.getPlayerConfig(target);
-                    pConfig.setBuildmode(!pConfig.isBuildmode());
-                    if (pConfig.isBuildmode()) {
-                        sender.sendMessage(target.getDisplayName() + " §7est désormais en mode build.");
-                        target.setPlayerListName("§7[Builder] " + target.getDisplayName());
-                        target.sendMessage("§cHRP : §7Vous êtes désormais en mode builder. Veuillez prendre en considération vos règles sur discord.");
-                    } else {
-                        sender.sendMessage(target.getDisplayName() + " §7n'est désormais plus en mode build.");
-                        target.setPlayerListName(target.getDisplayName());
-                        target.sendMessage("§cHRP : §7Vous n'êtes plus en mode builder. Merci de votre contribution pour Ninkai !");
-                    }
-                    newPermissionBuildMode(pConfig);
-                    break;
-                case "tempo":
-                    /* On lui retire */
-                    if (PlayerBuildTemp.contains(target.getName())) {
-                        PlayerBuildTemp.remove(target.getName());
-                        target.setGameMode(GameMode.SURVIVAL);
-                        target.getInventory().clear();
-                        target.sendMessage("§cHRP : §7Vous pouvez récupérer votre coffre en toute sécurité.");
-                        target.setPlayerListName(target.getDisplayName());
-                    }
-                    /* On lui remet */
-                    else {
-                        target.setPlayerListName("§7[Builder_TMP]" + target.getDisplayName());
-                        PlayerBuildTemp.add(target.getName());
-                        target.setGameMode(GameMode.CREATIVE);
-                        generateChest(target);
-                        target.sendMessage("§4IMPORTANT : §7Ne prenez pas votre inventaire avant de ne plus avoir le build mode temporaire ! (ou déco reco)");
-
-                    }
-                    break;
+            /* On lui retire */
+            if (PlayerBuildTemp.contains(target.getName())) {
+                PlayerBuildTemp.remove(target.getName());
+                target.setGameMode(GameMode.SURVIVAL);
+                target.getInventory().clear();
+                target.sendMessage("§cHRP : §7Vous pouvez récupérer votre coffre en toute sécurité.");
+                target.setPlayerListName(target.getDisplayName());
+            }
+            /* On lui remet */
+            else {
+                target.setPlayerListName("§7[Builder_TMP]" + target.getDisplayName());
+                PlayerBuildTemp.add(target.getName());
+                target.setGameMode(GameMode.CREATIVE);
+                generateChest(target);
+                target.sendMessage("§4IMPORTANT : §7Ne prenez pas votre inventaire avant de ne plus avoir le build mode temporaire ! (ou déco reco)");
             }
         } else {
-            sender.sendMessage("§cUsage : §7/build tempo|definitif (joueur)");
+            sender.sendMessage("§cUsage : §7/build (joueur)");
         }
     }
 
@@ -77,10 +58,6 @@ public class BuildCommand extends Command {
         List<String> completion = new ArrayList();
         switch (split.length) {
             case 1:
-                complete(completion, "tempo", split[0]);
-                complete(completion, "definitif", split[0]);
-                break;
-            case 2:
                 for (Player player : Main.plugin().getServer().getOnlinePlayers()) {
                     complete(completion, player.getName(), split[1]);
                 }
@@ -88,59 +65,6 @@ public class BuildCommand extends Command {
         }
         return completion;
     }
-
-    public static void newPermissionBuildMode(PlayerConfig pConfig) {
-        perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.command.me", false);
-        // C'est là pour l'auto complete
-        pConfig.getPlayer().updateCommands();
-        if (!pConfig.getPlayer().isOp()) {
-            if (pConfig.isBuildmode()) {
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("fawe.admin", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("worldedit.*", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.command.gamemode", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.gamemode", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.command.effect", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("multiverse.*", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("clickwarp.*", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.command.give", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.command.teleport", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.*", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.sniper", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.undouser", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.ignorelimitations", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.goto", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.brush.*", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("headdb.open", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("headdb.phead", true);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.debugstick", true);
-
-            } else {
-
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("fawe.admin", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("worldedit.*", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.command.gamemode", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.gamemode", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.command.effect", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("multiverse.*", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("clickwarp.*", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.command.give", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.command.teleport", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.*", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.sniper", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.undouser", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.ignorelimitations", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.goto", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("voxelsniper.brush.*", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("headdb.open", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("headdb.phead", false);
-                perms.get(pConfig.getPlayer().getUniqueId()).setPermission("minecraft.debugstick", false);
-
-            }
-        }
-        // C'est là pour l'auto complete
-        pConfig.getPlayer().updateCommands();
-    }
-
 
     private void generateChest(Player p) {
         org.bukkit.block.data.type.Chest chestData1;
